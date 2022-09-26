3d87b3a474c11482bdd0908f38fa6e9a829fe11d
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2022-09-26 16:01:18 -0400

    initial

---
+++ cypress.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'cypress'
+
+export default defineConfig({
+  e2e: {
+    env: {
+      grepFilterSpecs: true,
+      grepOmitFiltered: true,
+    },
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+      return require('./cypress/plugins/index.js')(on, config)
+    },
+    baseUrl: 'http://localhost:8888',
+    excludeSpecPattern: ['*.page.ts', 'utils.ts', '*.d.ts'],
+    specPattern: 'cypress/e2e/**/*spec.{js,ts}',
+  },
+})
